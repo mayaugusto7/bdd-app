7c71a5689a65b524efbf839725cdecfd92be0a60
Maycon Augusto <mayaugusto777@gmail.com>
2021-06-30 12:19:59 -0300

    Generate book scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_144033) do
+ActiveRecord::Schema.define(version: 2021_06_30_151753) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_06_30_144033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
